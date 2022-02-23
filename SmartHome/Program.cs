8f5104fe7884930787154a6f6f1ade5a63ebdcca
Ron Zhong <ron.zhong@helixleisure.com>
2022-02-23 12:11:47 +0800

    Add project files.

---
+++ SmartHome/Program.cs
@@ -0,0 +1,10 @@
+﻿using YeelightAPI;
+
+var device = new Device("192.168.18.20");
+await device.Connect();
+
+for (var i = 0; i < 10; ++i)
+{
+    await Task.Delay(1000);
+    await device.Toggle();
+}
